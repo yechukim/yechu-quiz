476ece4b6e37663c32d432ff7492bae006b3fee6
ykim02 <kye24601@gmail.com>
2021-04-25 20:40:32 +0900

    yechu quiz

---
+++ app.js
@@ -0,0 +1,35 @@
+const correct_answer = "B";
+const submit = document.querySelector('.btn');
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+const percent = document.querySelector('span');
+
+form.addEventListener('submit',(e)=> {
+    e.preventDefault();
+
+    let score = 0;
+    const answers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    answers.forEach((answer) => {
+        if(answer== correct_answer)
+        score +=25;
+
+    })
+    scrollTo(0,0); // x, y
+    result.classList.remove('d-none');
+
+    // scroll up when submit
+    //documnet 앞에 window 가 생략되어 있는 것 
+    //window => global object 
+
+    let output = 0
+    const timer = setInterval(()=> {
+         //override percent 
+    percent.textContent=`${output}%`;
+        if(output == score) {
+            clearInterval(timer);
+        }else {
+            output++;
+        }
+    },10);
+})
